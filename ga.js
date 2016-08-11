4941538b4f64cb607492d7dd6f4267a2e30f8c2f
maru0101 <nakamaruomaru0101@gmail.com>
2016-08-11 16:15:58 +0900

    add sns link

--- ga.js
+++ ga.js
@@ -5,3 +5,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-82290424-1', 'auto');
 ga('send', 'pageview');
+
+// twitter button
+window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
